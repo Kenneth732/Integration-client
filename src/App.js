bc9f5bc530499168c78cc66d92e8b86d58c1dca1
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-09-04 15:04:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [message, ]
+  const [animals, setAnimals] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/animals')
+      .then((res) => res.json())
+      .then((data) => setAnimals(data))
+      .catch((error) => console.error('Error:', error));
+  }, []);
+
   return (
-    <div className="App">
-      {/*  */}
+    <div className="app-container">
+      <ul className="animal-list">
+        {animals.map((animal, index) => (
+          <li className="animal-item animal-card" key={index}>
+            <img className="animal-image" src={animal.image} alt={animal.name} />
+            <div className="animal-details">
+              <p className="animal-name">Name: {animal.name}</p>
+              <p className="animal-type">Type: {animal.type}</p>
+              <p className="animal-weight">Weight: {animal.weight} lbs</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
