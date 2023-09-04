e8c970c750b4267a66cd46383e3757a2da331044
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-09-04 15:11:18 +0300

    form

--- src/App.js
+++ src/App.js
@@ -3,16 +3,78 @@ import './App.css';
 
 function App() {
   const [animals, setAnimals] = useState([]);
+  const [newAnimal, setNewAnimal] = useState({
+    name: '',
+    image: '',
+    type: '',
+    weight: '',
+  });
 
   useEffect(() => {
+    // Fetch the animals data when the component mounts
     fetch('/api/animals')
       .then((res) => res.json())
       .then((data) => setAnimals(data))
       .catch((error) => console.error('Error:', error));
   }, []);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Send a POST request to add the new animal
+    fetch('/api/animals', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newAnimal),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Update the animals state with the new animal
+        setAnimals([...animals, data]);
+        // Reset the form fields
+        setNewAnimal({
+          name: '',
+          image: '',
+          type: '',
+          weight: '',
+        });
+      })
+      .catch((error) => console.error('Error:', error));
+  };
+
   return (
-    <div className="app-container">
+    <div>
+      <h1>Animals</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          value={newAnimal.name}
+          onChange={(e) => setNewAnimal({ ...newAnimal, name: e.target.value })}
+        />
+        <label>Image URL:</label>
+        <input
+          type="text"
+          value={newAnimal.image}
+          onChange={(e) => setNewAnimal({ ...newAnimal, image: e.target.value })}
+        />
+        <label>Type:</label>
+        <input
+          type="text"
+          value={newAnimal.type}
+          onChange={(e) => setNewAnimal({ ...newAnimal, type: e.target.value })}
+        />
+        <label>Weight:</label>
+        <input
+          type="number"
+          value={newAnimal.weight}
+          onChange={(e) => setNewAnimal({ ...newAnimal, weight: e.target.value })}
+        />
+        <button type="submit">Add Animal</button>
+      </form>
+      <div className="app-container">
       <ul className="animal-list">
         {animals.map((animal, index) => (
           <li className="animal-item animal-card" key={index}>
@@ -25,6 +87,7 @@ function App() {
           </li>
         ))}
       </ul>
+    </div>
     </div>
   );
 }
